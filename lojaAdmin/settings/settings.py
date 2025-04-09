c69e08cd3f30879fe54f93b8b9537514128b8b9e
joaaopedromedeiros <joaopedroaraujodemedeiros@gmail.com>
2025-04-08 22:07:28 -0300

    feat : arquivos iniciais de settings e suas configurações

--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os # pode ser no inicio do codigo
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')# abaixo do BASE_DIR =
+Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -103,9 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
