ff97576a8320cb93ea7fb67386974fad10948b90
joaaopedromedeiros <joaopedroaraujodemedeiros@gmail.com>
2025-04-12 19:09:38 -0300

    finalizando capitulo 6

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from loja.models.Produto import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 
 # os produtos vão rodar e aparecer no terminal quando você acessar a página e não quando da run com o python file!!!!
 
@@ -10,18 +12,28 @@ def list_produto_view(request, id=None):
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
+    dias = request.GET.get("dias")
     produtos = Produto.objects.all()
+
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+        
+    #print(produtos)
+    if produto is not None:
+        produtos = produtos.filter(Produto=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria__Categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
     print(produtos)
     if id is None:
         id = 0
-    if destaque is not None:
-        print(destaque)
-    if produto is not None:
-        print(produto)
-    if promocao is not None:
-        print(promocao)
-    if categoria is not None:
-        print(categoria)
-    if fabricante is not None:
-        print(fabricante)
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    return HttpResponse('<h1>Produto de id %s!</h1> ' % id )
