509ecab357bafb992bcda251c9b55782f32d1134
joaaopedromedeiros <joaopedroaraujodemedeiros@gmail.com>
2025-04-18 17:42:52 -0300

    iniciando e finalizando captitulo 11 - login required

--- loja/urls/AuthUrls.py
+++ loja/urls/AuthUrls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from loja.views.AuthView import login_view
 from loja.views.AuthView import register_view
+from loja.views.AuthView import logout_view
+
 urlpatterns = [
-    path("login", login_view, name='login'),
-    path("register", register_view, name='register'),
+    path("login/", login_view, name='login'),
+    path("register/", register_view, name='register'),
+    path("logout", logout_view, name='logout'),
 ]
--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -3,6 +3,7 @@ from loja.models import Produto, Fabricante, Categoria
 from datetime import timedelta, datetime
 from django.utils import timezone
 from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import login_required
 
 # os produtos vão rodar e aparecer no terminal quando você acessar a página e não quando da run com o python file!!!!
 
@@ -42,6 +43,7 @@ def list_produto_view(request, id=None):
     }
     return render(request, template_name='produto/produto.html', context=context, status=200)
 
+@login_required
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
     fabricantes = Fabricante.objects.all()
